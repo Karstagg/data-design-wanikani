f2e5169bac5ce4f2c4674d9606409970ef53a95f
Matthew Fisher <m.r.f.48215@gmail.com>
2016-10-12 11:50:16 -0600

    added persona

--- public_html/index.php
+++ public_html/index.php
@@ -29,15 +29,35 @@
 			<!--begin section 1 (persona section)-->
 			<section>
 				<h2>Persona</h2>
-				<p></p>
+				<details>
+					<h4>Steve</h4>
+					<p>Steve is a 24 year old English teacher in Japan. He has a B.A. in English. He doesn't know Japanese that well and he isn't super motivated to study. He uses wanikani to study kanji and vocabulary. Steve is very familiar with internet browsers and uses them frequently.</p>
+					<p>Steve's goal is to increase his ability to read kanji and increase his vocabulary size in Japanese</p>
+					<h4>Info</h4>
+					<ul>
+						<li>Steve</li>
+						<li>Male</li>
+						<li>24 years old</li>
+						<li>English teacher in Japan</li>
+						<li>B.A. English</li>
+						<li>Doesn't know much Japanese</li>
+						<li>not great at studying</li>
+						<li>uses wanikani to study kanji and vocabulary</li>
+						<li>uses web browsers often</li>
+					</ul>
+				</details>
 			</section>
 			<section>
 				<h2>Use Case</h2>
-				<p></p>
+				<details>
+					<p></p>
+				</details>
 			</section>
 			<section>
 				<h2>Entities</h2>
-				<p></p>
+				<details>
+					<p></p>
+				</details>
 			</section>
 		</main>
 	</body>
