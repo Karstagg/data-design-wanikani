b6ae48f4a3e1f91ea53bf2d912048ea788a0c8f5
Matthew Fisher <m.r.f.48215@gmail.com>
2016-10-12 11:19:40 -0600

    added index.php and main.css

---
+++ public_html/index.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matthew
+ * Date: 10/12/2016
+ * Time: 11:00 AM
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/main.css" type="text/css" />
+		<link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/8bit-wonder" type="text/css"/>
+		<title>Data Design for Wanikani Radicals</title>
+	</head>
+	<body>
+		<!-- the header -->
+		<header>
+			<div class="header-main-text">
+				<h1>Data Design</h1>
+			</div>
+			<div class="header-sub-text">
+				<h4>For Wanikani profile and radicals</h4>
+			</div>
+		</header>
+		<!--main content-->
+		<main>
+			<!--begin section 1 (persona section)-->
+			<section>
+				<h2>Persona</h2>
+				<p></p>
+			</section>
+			<section>
+				<h2>Use Case</h2>
+				<p></p>
+			</section>
+			<section>
+				<h2>Entities</h2>
+				<p></p>
+			</section>
+		</main>
+	</body>
+</html>
